128085dd093e4ffd2144466f0f40bfabfb0f60b3
Zero <zzz8415@126.com>
2016-05-11 14:28:24 +0800

    新增TokenHelper

---
+++ Zero.Core/Web/TokenHelper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Zero.Core.Util;
+using Zero.Core.Extensions;
+
+namespace Zero.Core.Web
+{
+    /// <summary>
+    /// 网络授权帮助类(只支持所有属性为字符串类型的类)
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class TokenHelper
+    {
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        /// <returns></returns>
+        public static string Serialize<T>(T model) where T : new()
+        {
+            var type = typeof(T);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var p in type.GetProperties())
+            {
+                sb.Append(model.GetType().GetProperty(p.Name).GetValue(model, null).ToString() + "&");
+            }
+
+            return CryptoHelper.DES_Encrypt(sb.ToString().TrimEnd('&'));
+        }
+
+        /// <summary>
+        /// 反序列化
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public static T Deserialize<T>(string token) where T : new()
+        {
+            if (token.IsNullOrEmpty())
+                return default(T);
+            var sessionText = CryptoHelper.DES_Decrypt(token);
+            var tokens = sessionText.Split('&');
+
+            var type = typeof(T);
+
+            var ps = type.GetProperties();
+
+            if (tokens.Length != ps.Length)
+                return default(T);
+
+            var model = new T();
+
+            for(var i = 0; i < ps.Length; i++)
+            {
+                ps[i].SetValue(model, tokens[i], null);
+            }
+
+            return model;
+        }
+    }
+}
---
+++ Zero.Tests/Web/TokenHelperTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zero.Core.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zero.Core.Web.Tests
+{
+    [TestClass()]
+    public class TokenHelperTests
+    {
+        class Test
+        {
+            public string ID { get; set; }
+
+            public string IP { get; set; }
+
+            public string Ticks { get; set; }
+
+        }
+        [TestMethod()]
+        public void SerializeTest()
+        {
+            var test = new Test {
+                ID = "123",
+                IP = "2343",
+                Ticks = "333"
+            };
+
+            var token = TokenHelper.Serialize(test);
+            var test1 = TokenHelper.Deserialize<Test>(token);
+            Assert.Fail();
+        }
+    }
+}
