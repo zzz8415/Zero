305ffde27619bacee3f54ca10fdaa60b5aaaf4b1
Zero <zzz8415@126.com>
2018-10-30 16:39:45 +0800

    Created By Zero

---
+++ Zero.NETCore.Redis/RedisConfig.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zero.NETCore.Redis
+{
+    /// <summary>
+    /// Redis配置
+    /// </summary>
+    public class RedisConfig
+    {
+        /// <summary>
+        /// Host
+        /// </summary>
+        public string Host { get; set; }
+
+        /// <summary>
+        /// 端口
+        /// </summary>
+        public int Port { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// 长连接秒数
+        /// </summary>
+        public int KeepAlive { get; set; } = 30;
+
+        /// <summary>
+        /// 重试次数
+        /// </summary>
+        public int ConnectRetry { get; set; } = 3;
+
+        /// <summary>
+        /// 连接超时毫秒数
+        /// </summary>
+        public int ConnectTimeout { get; set; } = 3000;
+
+        /// <summary>
+        /// 响应超时毫秒数
+        /// </summary>
+        public int ResponseTimeout { get; set; } = 3000;
+
+        /// <summary>
+        /// 数据分片
+        /// </summary>
+        public int DBRegion { get; set; } = 0;
+    }
+}
