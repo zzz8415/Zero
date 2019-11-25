6072e743bfeab005a0223a554010f7131d4276e2
Zero <zzz8415@hotmail.com>
2019-11-25 19:37:02 +0800

    新增注入属性

---
+++ Zero.NETCore/Attribute/InjectAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zero.NETCore.Attribute
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class InjectAttribute : System.Attribute
+    {
+        public ServiceLifetime OptionsLifetime { get; set; } = ServiceLifetime.Scoped;
+    }
+}
