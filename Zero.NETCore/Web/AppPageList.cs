b9cc37cfd54fd76566ee83d68fa843bef52eb6ba
Zero <zzz8415@hotmail.com>
2019-11-26 20:52:32 +0800

    新增app分页类

---
+++ Zero.NETCore/Web/AppPageList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zero.NETCore.Web
+{
+    [Serializable]
+    public class AppPageList<T>
+    {
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <param name="RecordCount"></param>
+        public AppPageList(List<T> list, int pageSize)
+        {
+            this.List = list;
+
+            this.IsLastPage = this.List == null || this.List.Count <= pageSize;
+
+            if (!this.IsLastPage)
+            {
+                this.List = this.List.Take(pageSize).ToList();
+            }
+        }
+
+        /// <summary>
+        /// 数据列表
+        /// </summary>
+        public List<T> List { get; set; }
+
+        /// <summary>
+        /// 是否最后一页
+        /// </summary>
+        public bool IsLastPage { get; set; }
+    }
+}
