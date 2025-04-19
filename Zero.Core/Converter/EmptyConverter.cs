f38b82d49be758e760822b3d41588d3446db5447
virtualai2024 <virtualai2024@outlook.com>
2025-04-19 22:02:58 +0800

    ...

--- Zero.Core/Converter/EmptyConverter.cs
+++ Zero.Core/Converter/EmptyConverter.cs
@@ -15,27 +15,28 @@ namespace Zero.Core.Converter
 {
     public sealed class EmptyConverter(string dateFormat = null) : JsonConverterFactory
     {
-        public override bool CanConvert(Type typeToConvert)
-        {
-            return typeToConvert.IsValueType || // 值类型
-                   typeToConvert.IsArray || // 数组
-                   typeToConvert.IsGenericType; // 泛型
-        }
+        public override bool CanConvert(Type typeToConvert) => typeToConvert.IsValueType;
 
         public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
         {
-            var converterType = typeof(EmptyStringToDefaultConverter<>).MakeGenericType(typeToConvert);
+            if (typeToConvert.IsEnum)
+            {
+                var enumConverterType = typeof(EnumConverter<>).MakeGenericType(typeToConvert);
+                return (JsonConverter)Activator.CreateInstance(enumConverterType);
+            }
+
+            var converterType = typeof(EmptyStringConverter<>).MakeGenericType(typeToConvert);
             return (JsonConverter)Activator.CreateInstance(converterType, [options, dateFormat]);
         }
     }
 
-    public class EmptyStringToDefaultConverter<T> : JsonConverter<T>
+    public class EmptyStringConverter<T> : JsonConverter<T>
     {
         private readonly JsonSerializerOptions _safeOptions;
 
         private readonly string _dateFormat;
 
-        public EmptyStringToDefaultConverter(JsonSerializerOptions parentOptions, string dateFormat = null)
+        public EmptyStringConverter(JsonSerializerOptions parentOptions, string dateFormat = null)
         {
             // 克隆父级配置
             _safeOptions = new JsonSerializerOptions(parentOptions);
@@ -88,9 +89,40 @@ namespace Zero.Core.Converter
                     writer.WriteStringValue(((DateTime)(object)value).ToString(_dateFormat));
                     return;
                 }
+                else
+                {
+                    writer.WriteNullValue();
+                    return;
+                }
             }
 
             JsonSerializer.Serialize(writer, value, _safeOptions);
         }
     }
+    public class EnumConverter<TEnum> : JsonConverter<TEnum> where TEnum : struct, Enum
+    {
+        public override TEnum Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            switch (reader.TokenType)
+            {
+                case JsonTokenType.String:
+                    var stringValue = reader.GetString();
+                    if (Enum.TryParse<TEnum>(stringValue, ignoreCase: true, out var enumValue))
+                        return enumValue;
+                    break;
+
+                case JsonTokenType.Number when reader.TryGetUInt64(out var longValue):
+                    if (Enum.IsDefined(typeof(TEnum), longValue))
+                        return (TEnum)(object)longValue;
+                    break;
+            }
+            return default;
+        }
+
+        public override void Write(Utf8JsonWriter writer, TEnum value, JsonSerializerOptions options)
+        {
+            writer.WriteNumberValue(Convert.ToUInt64(value));
+        }
+    }
+
 }
